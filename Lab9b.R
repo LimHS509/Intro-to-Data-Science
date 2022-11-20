ad84058ca35633974a87c2bba4ef051ca7296a79
Hui Si Lim <huisilim520@gmail.com>
2022-11-20 21:05:49 +0800

    git updated

--- Lab9b.R
+++ Lab9b.R
@@ -1,3 +1,45 @@
 # Activity 1
+data(ToothGrowth)
+
+ToothGrowthdose <- as.factor(ToothGrowth$dose)
+head(ToothGrowth)
+
+df <- ToothGrowth[, -2]
+corr_mat <- round(cor(df),2)
+head(corr_mat)
+
+#Visualize Correlation Matrix using Correlogram
+library(corrplot)
+library(RColorBrewer)
+
+corrplot(corr_mat, type="upper")
+
+#Plotting Correlation with Heatmap
+library(lattice)
+library(reshape2)
+
+melted_corr_mat <- melt(corr_mat)
+head(melted_corr_mat)
+
+#Correlation Heatmap using ggplot2
+library(ggplot2)
+
+ggplot(data = melted_corr_mat, aes(x=Var1, y=Var2, fill=value)) + geom_tile() + geom_text(aes(Var2, Var1, label = value), color = "white", size = 4)
 
 # Activity 2
+scaled_data <- log(mtcars)
+print(scaled_data)
+
+scaled_data2 <- as.data.frame(scale(mtcars))
+print(scaled_data2)
+
+library(caret)
+
+minmax <- preProcess(as.data.frame(mtcars), method=c("range"))
+scaled_data3 <- predict(minmax, as.data.frame(mtcars))
+print(scaled_data3)
+
+summary(mtcars)
+summary(scaled_data)
+summary(scaled_data2)
+summary(scaled_data3)
---
+++ Quiz 3.R
@@ -0,0 +1,59 @@
+data("iris")
+#str <- structure
+str(iris)
+
+#ggplot3 <- for visualization
+#install.packages("ggplot2")
+library(ggplot2)
+
+#Building a plot
+ggplot(iris, aes(Petal.Length, Petal.Width)) + geom_point(aes(col=Species), size=4)
+ggplot(iris, aes(Sepal.Length, Sepal.Width)) + geom_point(aes(col=Species), size=4)
+
+#c(1, 2, 3, 4) <- column 1-4 and ignore column 5
+iris.new <- iris[, c(1, 2, 3, 4)]
+#"Species <- column 5
+iris.class <- iris[, "Species"]
+head(iris.new)
+head(iris.class)
+
+#3 <- volume of k / clusters = 3
+result <- kmeans(iris.new, 3)
+result$size
+result$centers
+
+#-5 <- take the rest column except 5th column
+iris_2 <- iris[, -5]
+#result$cluster <- outcome of kmeans algorithm
+iris_2$cluster <- as.character(result$cluster)
+
+ggplot() + geom_point(data = iris_2,
+                      mapping =aes(x = Petal.Width,
+                                  y = Petal.Length,
+                                  colour = cluster)) +
+  geom_point(mapping = aes_string(x = result$centers[, "Petal.Width"],
+                                  y = result$centers[, "Petal.Length"],
+                                  label = 1:3),
+             color = "black", size = 4) +
+  theme_light()
+
+table(result$cluster, iris.class)
+
+correct_classified = 36 + 47 + 50
+wrong_classified = 3 + 14
+acc = round(correct_classified / 150 * 100, digits = 2)
+print(paste("Total number of correctly classified instances are:", correct_classified))
+print(paste("Total number of wrong classified instance are:", wrong_classified))
+print(paste("The K-Means model's accuracy is:", acc))
+
+#install.packages("cluster")
+#cluster <- clustering algorithms
+library(cluster)
+#install.packages("factoextra")
+#factoextra <- clustering algorithms & visualization
+library(factoextra)
+
+#create plot of number of clusters vs total within sum of squares
+#fviz_nbclust <- determine the value of k
+fviz_nbclust(df, kmeans, method = "wss")
+
